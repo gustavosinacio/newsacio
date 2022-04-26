89b55ff06bdae0d1de411cbc78d1fe0099c5402c
Gustavo Inacio <gsinacio94@gmail.com>
2022-04-26 01:05:26 -0300

    adds sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,9 @@
+import styles from "../styles/home.module.scss";
+
 export default function Home() {
-  return <h1>Newsacio</h1>;
+  return (
+    <h1 className={styles.title}>
+      Newsacio <span>news</span>
+    </h1>
+  );
 }
